32fa55146302babf168ce7faa49eea417b34bafb
civilstudy2024@gmail.com <civilstudy2024@gmail.com>
2025-11-13 22:48:05 +0530

    deploy link add

--- rag_chatbot/components/Header.jsx
+++ rag_chatbot/components/Header.jsx
@@ -119,7 +119,7 @@ export default function RAGChatbot() {
     setIsLoading(true);
 
     try {
-      const res = await fetch("http://localhost:3000/api/chat", {
+      const res = await  fetch(`${process.env.NEXT_PUBLIC_APP_URL}/api/chat`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
